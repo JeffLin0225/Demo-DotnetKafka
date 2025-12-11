3a9e5add067f98c23dcf4e1c45ab40c56477dd38
JeffLin0225 <jefflin0225@gmail.com>
2025-12-12 01:17:13 +0800

    [new]Producer高效能interface

--- demo/Common/Kafka/KafkaProducer.cs
+++ demo/Common/Kafka/KafkaProducer.cs
@@ -1,6 +1,37 @@
-namespace demo.Common.Kafka;
+using Confluent.Kafka;
 
-public class KafkaProducer
+namespace demo.Common.Kafka;
+using demo.Common.Kafka;
+
+public interface IKafkaProducerService
 {
+    Task SendAsync(string topic , string message);
+}
+public class KafkaProducer : IKafkaProducerService
+{
+    private readonly ILogger<KafkaProducer> _logger;
+    private readonly KafkaFactory _kafkaFactory;
+
+    public KafkaProducer(ILogger<KafkaProducer> logger , KafkaFactory kafkaFactory)
+    {
+        _logger = logger;
+        _kafkaFactory = kafkaFactory;
+    }
     
+    /// <summary>
+    /// Producer
+    /// </summary>
+    public async Task SendAsync(string topic , string message)
+    {
+        var producer = _kafkaFactory.CreateProducer();
+        try
+        {
+            // 發送等待
+            await producer.ProduceAsync(topic, new Message<Null, string>{Value = message}); 
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Kafka SendAsync Error: {e}" , e.Message);
+        }
+    }
 }
